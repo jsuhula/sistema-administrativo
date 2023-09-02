9536797f13d5c19fb00741eb9a7805acd6295e29
jsuhula <jsuhula@miumg.edu.gt>
2023-09-02 06:06:59 +0000

    [ADD] Modulo Gestion Platillos

--- views/gestion-platillos.php
+++ views/gestion-platillos.php
@@ -1,110 +1,105 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administración</title>
+    <title>Gestión de Platillos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
 </head>
 
-<body>
-
+<body class="bg-light">
     <header class="bg-dark text-white text-center py-4">
-        <h1 class="display-4">Administración Restaurante</h1>
+        <h1 class="display-4">Gestión de Platillos</h1>
     </header>
 
-    <!-- Barra de navegación -->
+    <!-- Barra de navegación con estilos de Bootstrap -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <span class="nav-link"><i class="fas fa-user"></i> User</span>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#caja">Caja</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#usuarios">Usuarios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="views/gestion-platillos.php">Platillos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#reportes">Reportes</a>
-                    </li>
-                    <li class="nav-item text-white">
-                        <a class="nav-link" href="views/menu.php">Menú</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#salir">
-                        <i class="fas fa-sign-out-alt"></i> Salir
-                    </a>
-                </li>
-            </div>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="../index.php">Regresar</a></li>
+            </ul>
         </div>
     </nav>
+    <main class="container mt-4">
+        <section id="menu" class="bg-white rounded-4 shadow p-4">
+            <h2 class="mb-4">Menú Actual</h2>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Buscar platillo" aria-label="Buscar platillo"
+                    aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+            <div class="menu-container" style="max-height: 300px; overflow-y: auto;">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Precio</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr data-nombre="Platillo 1" data-descripcion="Descripción del platillo 1" data-precio="$10.99">
+                            <td>Platillo 1</td>
+                            <td>Descripción del platillo 1</td>
+                            <td>Q10.99</td>
+                            <td>
+                                <button class="btn btn-link" data-toggle="modal" data-target="#editarModal"
+                                    data-nombre="Platillo 1" data-descripcion="Descripción del platillo 1"
+                                    data-precio="$10.99">
+                                    <i class="fas fa-edit"></i>
+                                </button>
+                                <button class="btn btn-link" data-toggle="modal" data-target="#eliminarModal"
+                                    data-nombre="Platillo 1">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <tr data-nombre="Platillo 2" data-descripcion="Descripción del platillo 2" data-precio="$12.99">
+                            <td>Platillo 2</td>
+                            <td>Descripción del platillo 2</td>
+                            <td>Q.12.99</td>
+                            <td>
+                                <button class="btn btn-link" data-toggle="modal" data-target="#editarModal"
+                                    data-nombre="Platillo 2" data-descripcion="Descripción del platillo 2"
+                                    data-precio="$12.99">
+                                    <i class="fas fa-edit"></i>
+                                </button>
+                                <button class="btn btn-link" data-toggle="modal" data-target="#eliminarModal"
+                                    data-nombre="Platillo 2">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
 
-    <div class="container shadow-lg mt-5 mb-5 p-md-5 rounded-4">
-        <h2 class="text-center pt-3">Estadisticas del mes</h2>
-        <div class="row mt-4">
-            <div class="col-md-6 p-2">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h2 class="card-title">Detalles de Ventas</h2>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Fecha</th>
-                                    <th>Total de Ventas</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>01/09/2023</td>
-                                    <td>Q.1200</td>
-                                </tr>
-                                <tr>
-                                    <td>02/09/2023</td>
-                                    <td>Q.1400</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                    </tbody>
+                </table>
             </div>
-            <div class="col-md-6 p-2">
-                <div class="card h-100">
-                    <div class="card-body lh-sm">
-                        <h2 class="card-title">Platillos Más Vendido</h2>
-                        <p>Los platillos más vendidos este mes:</p>
-                        <p><strong>Pizza de Pepperoni</strong></p>
-                        <p><strong>Pizza de Pepperoni</strong></p>
-                        <p><strong>Pizza de Pepperoni</strong></p>
-                    </div>
+
+        </section>
+        <section id="agregar" class="bg-white rounded-4 shadow mt-5 mb-5 p-4 ">
+            <h2 class="mb-4">Agregar Nuevo Platillo</h2>
+            <form>
+                <div class="mb-3">
+                    <label for="nombre" class="form-label">Nombre del Platillo:</label>
+                    <input type="text" class="form-control" id="nombre" required>
                 </div>
-            </div>
-        </div>
-        <div class="row mt-4 justify-content-center">
-            <div class="col-md-10 pb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h2 class="card-title">Gráfica de Ventas Mensuales</h2>
-                        <canvas id="ventasMensuales"></canvas>
-                    </div>
+                <div class="mb-3">
+                    <label for="descripcion" class="form-label">Descripción:</label>
+                    <textarea class="form-control" id="descripcion" rows="4" required></textarea>
                 </div>
-            </div>
-        </div>
-    </div>
+                <div class="mb-3">
+                    <label for="precio" class="form-label">Precio:</label>
+                    <input type="number" class="form-control" id="precio" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Agregar Platillo</button>
+            </form>
+        </section>
+    </main>
     <footer class="bg-dark text-white text-center">
         <div class="container">
             <div class="row">
@@ -120,33 +115,9 @@
 </body>
 
 </html>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"></script>
-
-<!-- Configura la gráfica de ventas mensuales -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://unpkg.com/@popperjs/core@2"></script>
 <script>
-    var ctx = document.getElementById('ventasMensuales').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
-            datasets: [{
-                label: 'Ventas Mensuales',
-                data: [1200, 1400, 1600, 1800, 2000, 2200],
-                fill: false,
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 2
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
     document.addEventListener('DOMContentLoaded', function () {
         const navbar = document.querySelector('.navbar');
         const header = document.querySelector('header');
