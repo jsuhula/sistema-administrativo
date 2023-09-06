8b1636714b4ac7b4920d5f75b893a220d87a56f9
jsuhula <jsuhula@miumg.edu.gt>
2023-09-05 20:38:44 -0600

    [FIX] Vista con opcion de seleccionar bebida en la forma de menu

--- views/menu.php
+++ views/menu.php
@@ -112,6 +112,6 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
