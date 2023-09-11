e3918e79100acbbd290c6c264b0c57e585bd7178
jsuhula <jsuhula@miumg.edu.gt>
2023-09-11 21:35:05 +0000

    [ADD] Se agrego el modulo Gestion de Nomina

--- views/gestion-nomina.php
+++ views/gestion-nomina.php
@@ -14,16 +14,18 @@
         <span class="display-4">Gestión de Nómina</span>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li>
-                            <a id="regresar" class="nav-link" href="/index.php"><i class="fas fa-arrow-left"></i> Regresar</a>
+                            <a id="regresar" class="nav-link" href="/index.php"><i class="fas fa-arrow-left"></i>
+                                Regresar</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#salarios" data-section="salarios">Empleados</a>
+                            <a class="nav-link" href="#empleados" data-section="empleados">Empleados</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#informes" data-section="informes">Informes</a>
@@ -31,7 +33,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#comisiones" data-section="comisiones">Comisiones</a>
                         </li>
-                        
+
                         <li class="nav-item">
                             <a class="nav-link" href="#liquidacion" data-section="liquidacion">Liquidacion</a>
                         </li>
@@ -45,16 +47,18 @@
     </header>
 
     <main class="container mt-4">
-        <section id="salarios" class="container bg-white shadow-lg mt-5 mb-5 p-md-5 rounded-4">
-        <h2>Empleados</h2>
-        <div class="input-group mb-3">
-            <input id="nombre-empleado" type="text" class="form-control" placeholder="Buscar por Nombre" aria-label="Buscar Empleado" aria-describedby="button-addon2">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2">
-                <i class="fas fa-search"></i>
-            </button>
-        </div>
-            
-            <!-- Lista de empleados -->
+
+        <!-- Empleados -->
+        <section id="empleados" class="container bg-white shadow-lg mt-5 mb-5 p-md-5 rounded-4">
+            <h2>Empleados</h2>
+            <div class="input-group mb-3">
+                <input id="nombre-empleado" type="text" class="form-control" placeholder="Buscar por Nombre"
+                    aria-label="Buscar Empleado" aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+
             <h3>Lista de Empleados</h3>
             <table class="table table-striped">
                 <thead class="sticky-top">
@@ -77,10 +81,12 @@
                         <td>$2500.00</td>
                         <td>Activo</td>
                         <td>
-                            <button class="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#editarEmpleadoModal">
+                            <button class="btn btn-sm btn-success" data-bs-toggle="modal"
+                                data-bs-target="#editarEmpleadoModal">
                                 <i class="fas fa-edit"></i>
                             </button>
-                            <button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#eliminarUsuarioModal">
+                            <button class="btn btn-sm btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#eliminarUsuarioModal">
                                 <i class="fas fa-trash"></i>
                             </button>
                         </td>
@@ -93,10 +99,12 @@
                         <td>$2500.00</td>
                         <td>Activo</td>
                         <td>
-                            <button class="btn btn-sm btn-success" data-bs-toggle="modal" data-bs-target="#editarEmpleadoModal">
+                            <button class="btn btn-sm btn-success" data-bs-toggle="modal"
+                                data-bs-target="#editarEmpleadoModal">
                                 <i class="fas fa-edit"></i>
                             </button>
-                            <button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#eliminarUsuarioModal">
+                            <button class="btn btn-sm btn-danger" data-bs-toggle="modal"
+                                data-bs-target="#eliminarUsuarioModal">
                                 <i class="fas fa-trash"></i>
                             </button>
                         </td>
@@ -166,7 +174,7 @@
         <!-- Calculo de liquidacion -->
         <section id="liquidacion" class="container bg-white shadow-lg mt-5 mb-5 p-md-5 rounded-4" hidden>
             <h2>Cálculo de Liquidación</h2>
-            
+
             <div class="mb-4">
                 <label for="empleados-inactivos" class="form-label">Selecciona un empleado:</label>
                 <select class="form-select" id="empleados-inactivos">
@@ -177,7 +185,7 @@
             </div>
             <div>
                 <caption class="caption-top">General</caption>
-                <table class="table table-striped mt-4" id="tabla-liquidacion" >
+                <table class="table table-striped mt-4" id="tabla-liquidacion">
                     <thead>
                         <tr>
                             <th>Empleado</th>
@@ -202,7 +210,7 @@
             </div>
             <div>
                 <caption class="caption-top">Vacaciones</caption>
-                <table class="table table-striped mt-4" id="tabla-liquidacion" >
+                <table class="table table-striped mt-4" id="tabla-liquidacion">
                     <thead>
                         <tr>
                             <th>Empleado</th>
@@ -228,7 +236,7 @@
             <button class="btn btn-success" id="generar-liquidacion">Generar Liquidación</button>
         </section>
         <section id="resumen" class="container bg-white shadow-lg mt-5 mb-5 p-md-5 rounded-4" hidden>
-            <h2 class="text-center">Resumen por Conceptos</h2>
+            <h2>Resumen por Conceptos</h2>
             <table class="table table-striped">
                 <thead class="sticky-top">
                     <tr>
@@ -266,10 +274,41 @@
                 </tbody>
             </table>
         </section>
+        <section id="comisiones" class="container bg-white shadow-lg mt-5 mb-5 p-md-5 rounded-4" hidden>
+            <h2>Comisiones de Meseros y Chefs</h2>
 
+            <div class="mb-3">
+                <label for="tipoComision" class="form-label">Selecciona el tipo de comisión:</label>
+                <select class="form-select" id="tipoComision" onchange="mostrarComisiones()">
+                    <option value="mesero">Mesero</option>
+                    <option value="chef">Chef</option>
+                </select>
+            </div>
+
+            <table class="table table-striped" id="tablaComisiones">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Monto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mesero/Chef</td>
+                        <td>Q.1,100.00</td>
+                    </tr>
+                    <tr>
+                        <td>Mesero/Chef</td>
+                        <td>Q.1,120.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     </main>
+
     <!-- Modal para editar empleado -->
-    <div class="modal fade" id="editarEmpleadoModal" tabindex="-1" aria-labelledby="editarEmpleadoModalLabel" aria-hidden="true">
+    <div class="modal fade" id="editarEmpleadoModal" tabindex="-1" aria-labelledby="editarEmpleadoModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
@@ -296,13 +335,15 @@
                             </div>
                             <div class="col-md-6">
                                 <label for="fechaNacimiento" class="form-label">Fecha de Nacimiento:</label>
-                                <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimiento" required>
+                                <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimiento"
+                                    required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
                                 <label for="identificacion" class="form-label">DPI:</label>
-                                <input type="text" class="form-control" id="identificacion" name="identificacion" required>
+                                <input type="text" class="form-control" id="identificacion" name="identificacion"
+                                    required>
                             </div>
                             <div class="col-md-6">
                                 <label for="nit" class="form-label">NIT:</label>
@@ -314,12 +355,12 @@
                                 <label for="correo" class="form-label">Correo Electrónico:</label>
                                 <input type="email" class="form-control" id="correo" name="correo">
                             </div>
-                            
+
                             <div class="col-md-6">
                                 <label for="telefono" class="form-label">Número de Teléfono:</label>
                                 <input type="text" class="form-control" id="telefono" name="telefono">
                             </div>
-                            
+
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
@@ -380,9 +421,9 @@
         </div>
     </div>
 
-
     <!-- Modal de Eliminación de Usuario -->
-    <div class="modal fade" id="eliminarUsuarioModal" tabindex="-1" aria-labelledby="eliminarUsuarioModalLabel" aria-hidden="true">
+    <div class="modal fade" id="eliminarUsuarioModal" tabindex="-1" aria-labelledby="eliminarUsuarioModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -402,51 +443,62 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const header = document.querySelector('header');
-            const headerHeight = header.offsetHeight;
-            const salarios = document.getElementById('salarios');
-            const informes = document.getElementById('informes');
-            const liquidaciones = document.getElementById('liquidacion');
-            const resumen = document.getElementById('resumen');
-            const regresarLink = document.getElementById('regresar');
+    document.addEventListener('DOMContentLoaded', function() {
+        const header = document.querySelector('header');
+        const headerHeight = header.offsetHeight;
+        const empleados = document.getElementById('empleados');
+        const informes = document.getElementById('informes');
+        const liquidaciones = document.getElementById('liquidacion');
+        const resumen = document.getElementById('resumen');
+        const comisiones = document.getElementById('comisiones');
+        const regresarLink = document.getElementById('regresar');
 
-            const navLinks = document.querySelectorAll('.navbar-nav a.nav-link');
+        const navLinks = document.querySelectorAll('.navbar-nav a.nav-link');
 
-            navLinks.forEach(link => {
-                link.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    const targetId = this.getAttribute('href').substring(1);
-
-                    if (targetId === 'salarios') {
-                        salarios.removeAttribute('hidden');
-                        informes.setAttribute('hidden', 'true');
-                        liquidaciones.setAttribute('hidden', 'true');
-                    } else if (targetId === 'informes') {
-                        informes.removeAttribute('hidden');
-                        salarios.setAttribute('hidden', 'true');
-                        liquidaciones.setAttribute('hidden', 'true');
-                        resumen.setAttribute('hidden', 'true');
-                    } else if(targetId === 'liquidacion'){
-                        liquidaciones.removeAttribute('hidden');
-                        informes.setAttribute('hidden', 'true');
-                        salarios.setAttribute('hidden', 'true');
-                        resumen.setAttribute('hidden', 'true');
-                    } else if(targetId === 'resumen'){
-                        informes.setAttribute('hidden', 'true');
-                        salarios.setAttribute('hidden', 'true');
-                        liquidaciones.setAttribute('hidden', 'true');
-                        resumen.removeAttribute('hidden');
-                    }
-                });
-            });
-
-            regresarLink.addEventListener('click', function(event) {
+        navLinks.forEach(link => {
+            link.addEventListener('click', function(event) {
                 event.preventDefault();
+                const targetId = this.getAttribute('href').substring(1);
 
-                window.location.href = '../index.php';
+                if (targetId === 'empleados') {
+                    empleados.removeAttribute('hidden');
+                    informes.setAttribute('hidden', 'true');
+                    liquidaciones.setAttribute('hidden', 'true');
+                    comisiones.setAttribute('hidden', 'true');
+                } else if (targetId === 'informes') {
+                    informes.removeAttribute('hidden');
+                    empleados.setAttribute('hidden', 'true');
+                    liquidaciones.setAttribute('hidden', 'true');
+                    resumen.setAttribute('hidden', 'true');
+                    comisiones.setAttribute('hidden', 'true');
+                } else if (targetId === 'liquidacion') {
+                    liquidaciones.removeAttribute('hidden');
+                    informes.setAttribute('hidden', 'true');
+                    empleados.setAttribute('hidden', 'true');
+                    resumen.setAttribute('hidden', 'true');
+                    comisiones.setAttribute('hidden', 'true');
+                } else if (targetId === 'resumen') {
+                    informes.setAttribute('hidden', 'true');
+                    empleados.setAttribute('hidden', 'true');
+                    liquidaciones.setAttribute('hidden', 'true');
+                    resumen.removeAttribute('hidden');
+                    comisiones.setAttribute('hidden', 'true');
+                } else if (targetId === 'comisiones') {
+                    informes.setAttribute('hidden', 'true');
+                    empleados.setAttribute('hidden', 'true');
+                    liquidaciones.setAttribute('hidden', 'true');
+                    resumen.setAttribute('hidden', 'true');
+                    comisiones.removeAttribute('hidden');
+                }
             });
         });
+
+        regresarLink.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            window.location.href = '../index.php';
+        });
+    });
     </script>
 </body>
 
