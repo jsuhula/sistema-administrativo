00f28ecf5076dfac7b384fca435e5b0ff172cc1b
jsuhula <jsuhula@miumg.edu.gt>
2023-09-26 22:56:40 +0000

    [FIX] Se corrigen las credenciales de la base de datos

--- includes/db-connector.php
+++ includes/db-connector.php
@@ -3,11 +3,11 @@
 class MySQLConnector{
     private $connector;
     private $server = "localhost";
-    private $dbname = "deaquisoy";
-    private $user = "root";
-    private $passwd = "";
-    public function __construct(){
+    private $dbname = "id21238446_deaquisoy";
+    private $user = "id21238446_admin";
+    private $passwd = "Admin@root1";
 
+    public function __construct(){
         try{
             $this->connector = new PDO("mysql:host={$this->server};dbname={$this->dbname}", $this->user, $this->passwd);
             $this->connector->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
