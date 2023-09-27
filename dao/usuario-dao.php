408e592a5c6fa185b8c6ecba679bafcd1e54e6d1
JSuhul <tec.desarrollo32.gtd@gmail.com>
2023-09-27 08:23:42 -0600

    [FIX] Logica de Usuarios en la capa de acceso a datos

--- dao/usuario-dao.php
+++ dao/usuario-dao.php
@@ -8,36 +8,74 @@ class UserDAO
         $this->connection = new MySQLConnector();
         $this->connection = $this->connection->getConnection();
     }
-    public function ingresarEmpleado(string $nombre, string $correo)
+    public function guardarUsuario(string $email, string $clave, int $rol)
     {
-        $query = "INSERT INTO usuarios(nombre, correo) VALUES(?, ?)";
+        $query = "call spRegistrarUsuario (?, ?, ?)";
         $prpstmt = $this->connection->prepare($query);
-        $prpstmt->bindParam(1, $nombre);
-        $prpstmt->bindParam(2, $correo);
+        $prpstmt->bindParam(1, $email);
+        $prpstmt->bindParam(2, $clave);
+        $prpstmt->bindParam(3, $rol);
         $prpstmt->execute();
         return $prpstmt;
     }
-    public function listarEmpleados(){
-        $query = "SELECT * FROM Empleado";
+    public function actualizarUsuario(int $codigoUsuarioSistema, $email, $clave, $rol)
+    {
+        $query = "UPDATE UsuarioSistema SET Email = ?, Clave = ?, CodigoRol = ? WHERE CodigoUsuarioSistema = ?";
         $prpstmt = $this->connection->prepare($query);
+        $prpstmt->bindParam(1, $email);
+        $prpstmt->bindParam(2, $clave);
+        $prpstmt->bindParam(3, $rol);
+        $prpstmt->bindParam(4, $codigoUsuarioSistema);
         $prpstmt->execute();
         return $prpstmt;
     }
-    public function listarUsuarios(){
+    public function actualizarUsuarioNoClave(int $codigoUsuarioSistema, $email, $rol)
+    {
+        $query = "UPDATE UsuarioSistema SET Email = ?, CodigoRol = ? WHERE CodigoUsuarioSistema = ?";
+        $prpstmt = $this->connection->prepare($query);
+        $prpstmt->bindParam(1, $email);
+        $prpstmt->bindParam(2, $rol);
+        $prpstmt->bindParam(3, $codigoUsuarioSistema);
+        $prpstmt->execute();
+        return $prpstmt;
+    }
+    public function eliminarUsuario(int $codigoUsuarioSistema)
+    {
+        $query = "DELETE FROM UsuarioSistema WHERE CodigoUsuarioSistema = ?";
+        $prpstmt = $this->connection->prepare($query);
+        $prpstmt->bindParam(1, $codigoUsuarioSistema);
+        $prpstmt->execute();
+        return $prpstmt;
+    }
+    public function listarUsuarios()
+    {
         $query = "SELECT US.CodigoUsuarioSistema AS Codigo
                             , US.Email
                             , RO.Nombre AS Rol
-                            , RO.GestionaNomina AS Nomina
-                            , RO.GestionaEmpleados AS Empleados
-                            , RO.GestionaMenu AS Menu
-                            , RO.GestionaReportes AS Reportes
-                            , RO.GestionaCaja AS Caja
+                            , RO.CodigoRol
                     FROM `UsuarioSistema` AS US
                     INNER JOIN Rol AS RO ON RO.CodigoRol = US.CodigoRol";
         $prpstmt = $this->connection->prepare($query);
         $prpstmt->execute();
         return $prpstmt;
     }
+
+    public function listarRoles()
+    {
+        $query = "SELECT CodigoRol
+                        , Nombre
+                        , GestionaNomina
+                        , GestionaEmpleados
+                        , GestionaMenu
+                        , GestionaReportes
+                        , GestionaCaja
+                        , Asistencia
+                   FROM Rol";
+        $prpstmt = $this->connection->prepare($query);
+        $prpstmt->execute();
+        return $prpstmt;
+    }
+
 }
 
 ?>
