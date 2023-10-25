843088f5ea653b3a0f53465a93a62337aa5fc4e8
jsuhula <jsuhula@miumg.edu.gt>
2023-10-25 01:02:46 -0600

    [FIX] Se corrigen algunos elementos semanticos

--- login.php
+++ login.php
@@ -12,6 +12,7 @@ if ($_SESSION) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="resources/food.svg" type="image/svg+xml">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
   <title>Ingresar</title>
