4eecbc84baafd15ff34d420cc94f8fda95a45604
jsuhula <jsuhula@miumg.edu.gt>
2023-10-24 21:57:25 -0600

    [FIX] Se corrigen algunos elementos semanticos

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_POST['asistenciaEntrada'])) {
     $usuarioAsistencia->asistenciaSalida($_SESSION['CodigoUsuario'], strval($fecha));
 }
 
-$asistencia = $usuarioAsistencia->validarAsistencia($_SESSION['CodigoUsuario'], strval(date("d-m-Y")))->fetch(PDO::FETCH_OBJ);
+$asistencia = $usuarioAsistencia->validarAsistencia($_SESSION['CodigoUsuario'], strval(date("Y-m-d")))->fetch(PDO::FETCH_OBJ);
 $nombreUsuarioActual = empty($usuarioSesion->NombreUsuarioSesion) ? $usuarioSesion->UsuarioEmail : $usuarioSesion->NombreUsuarioSesion;
 
 ?>
